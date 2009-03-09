f6452c9d18db18153c8bc116e862c519723b0df3
The Android Open Source Project <initial-contribution@android.com>
2009-03-09 11:52:14 -0700

    auto import from //branches/cupcake/...@137197

--- Android.mk
+++ Android.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := eng development
+LOCAL_MODULE_TAGS := user # should be optional, but launcher crashes without this
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
